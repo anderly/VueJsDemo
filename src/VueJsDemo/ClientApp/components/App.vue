2e24bd401483250ae69babce8f055805634a01f7
Adam Anderly <adam@anderly.com>
2017-01-16 17:17:40 -0600

    Layout styling changes.

--- src/VueJsDemo/ClientApp/components/App.vue
+++ src/VueJsDemo/ClientApp/components/App.vue
@@ -1,78 +1,73 @@
 ﻿<template>
     <div>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a asp-area="" asp-controller="Home" asp-action="Index" class="navbar-brand">VueJsDemo</a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li v-if="!auth.user.authenticated">
-                        <router-link :to="{ name: 'home' }">Home</router-link>
-                    </li>
-                    <li v-if="auth.user.authenticated">
-                        <router-link :to="{ name: 'dashboard' }">Home</router-link>
-                    </li>
-                    <!--<li class="pull-right" v-if="!auth.user.authenticated">
-        <router-link :to="{ name: 'signin' }">Sign in</router-link>
-    </li>
-    <li class="pull-right" v-if="auth.user.authenticated">
-        <a href="javascript:void(0)" @click="signout">Sign out</a>
-    </li>-->
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li v-if="!auth.user.authenticated">
-                        <router-link :to="{ name: 'register' }">Register</router-link>
-                    </li>
-                    <li v-if="!auth.user.authenticated">
-                        <a href="javascript:void(0)" @click="signin">Sign in</a>
-                    </li>
-                    <li class="dropdown">
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a asp-area="" asp-controller="Home" asp-action="Index" class="navbar-brand">VueJsDemo</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li v-if="!auth.user.authenticated">
+                            <router-link :to="{ name: 'home' }">Home</router-link>
+                        </li>
+                        <li v-if="auth.user.authenticated">
+                            <router-link :to="{ name: 'dashboard' }">Home</router-link>
+                        </li>
+                        <!--<li class="pull-right" v-if="!auth.user.authenticated">
+            <router-link :to="{ name: 'signin' }">Sign in</router-link>
+        </li>
+        <li class="pull-right" v-if="auth.user.authenticated">
+            <a href="javascript:void(0)" @click="signout">Sign out</a>
+        </li>-->
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li v-if="!auth.user.authenticated">
+                            <router-link :to="{ name: 'register' }">Register</router-link>
+                        </li>
+                        <li v-if="!auth.user.authenticated">
+                            <a href="javascript:void(0)" @click="signin">Sign in</a>
+                        </li>
+                        <li class="dropdown">
 
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" v-if="auth.user.authenticated">
-                            <i class="icon-user"></i>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" v-if="auth.user.authenticated">
+                                <i class="icon-user"></i>
+                                Hi, {{ auth.user.profile.name }}
+                                <b class="caret"></b>
+                            </a>
+
+                            <ul class="dropdown-menu">
+                                <li v-if="auth.user.authenticated">
+                                    <a href="javascript:void(0)" @click="signout">Sign out</a>
+                                </li>
+                            </ul>
+
+                        </li>
+                        <!--<li class="pull-right" v-if="!auth.user.authenticated">
+                            <a href="javascript:void(0)" @click="signin">Sign in</a>
+                        </li>
+                        <li class="pull-right" v-if="auth.user.authenticated">
+                            <a href="javascript:void(0)" @click="signout">Sign out</a>
+                        </li>
+                        <li class="pull-right" v-if="auth.user.authenticated">
                             Hi, {{ auth.user.profile.name }}
-                            <b class="caret"></b>
-                        </a>
-
-                        <ul class="dropdown-menu">
-                            <li><a href="UserProfile" target="main" onclick="$('#breadcrumb').show();NextScreen('My Profile', 'UserProfile', true);top.resize();">My Profile</a></li>
-                            <li>
-                                <a href="My_Preferences.asp" target="main" onclick="$('#breadcrumb').show();NextScreen('My Preferences', 'My_Preferences.asp', true);top.resize();">My Preferences</a>
-                                <!--<li class="divider"></li>-->
-                            </li>
-                            <li class="pull-right" v-if="auth.user.authenticated">
-                                <a href="javascript:void(0)" @click="signout">Sign out</a>
-                            </li>
-                        </ul>
-
-                    </li>
-                    <!--<li class="pull-right" v-if="!auth.user.authenticated">
-                        <a href="javascript:void(0)" @click="signin">Sign in</a>
-                    </li>
-                    <li class="pull-right" v-if="auth.user.authenticated">
-                        <a href="javascript:void(0)" @click="signout">Sign out</a>
-                    </li>
-                    <li class="pull-right" v-if="auth.user.authenticated">
-                        Hi, {{ auth.user.profile.name }}
-                    </li>-->
-                </ul>
+                        </li>-->
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container body-content">
-        <router-view></router-view>
-    </div>
-        <hr />
-        <footer>
-            <p>&copy; 2017 - VueJsDemo</p>
-        </footer>
+        <div class="container body-content">
+            <router-view></router-view>
+            <hr />
+            <footer>
+                <p>&copy; 2017 - VueJsDemo</p>
+            </footer>
+        </div>
     </div>
 </template>
 
