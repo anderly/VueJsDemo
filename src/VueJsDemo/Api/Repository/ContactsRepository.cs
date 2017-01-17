991bb3a607c0f6ca6ff2cda6693678d76e898b60
Adam Anderly <adam@anderly.com>
2017-01-17 14:46:32 -0600

    Cleaned up repository.

--- src/VueJsDemo/Api/Repository/ContactsRepository.cs
+++ src/VueJsDemo/Api/Repository/ContactsRepository.cs
@@ -9,68 +9,44 @@ namespace VueJsDemo.Api.Repository
 {
     public class ContactsRepository : IContactsRepository
     {
-        AppDbContext _dbContext;
+        private readonly AppDbContext _dbContext;
 
         public ContactsRepository(AppDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-        static List<Contact> ContactsList = new List<Contact>();
 
-        public async Task AddAsync(Contact item)
+        public async Task AddAsync(Contact contact)
         {
-            //ContactsList.Add(item);
-            _dbContext.Contacts.Add(item);
+            _dbContext.Contacts.Add(contact);
             await _dbContext.SaveChangesAsync();
         }
         public async Task<Contact> GetByIdAsync(string key)
         {
             // ToDo - Integrate with EF Core, DbSet.Find not available yet
-            //return ContactsList
-            //    .Where(e => e.MobilePhone.Equals(key))
-            //    .SingleOrDefault();
             return await _dbContext.Contacts
                 .SingleOrDefaultAsync(c => c.MobilePhone.Equals(key));
         }
 
         public async Task<IEnumerable<Contact>> ListAsync()
         {
-            //ContactsList.Add(new Contacts() {FirstName ="Mithun", MobilePhone = "2345" });
-
-            return await _dbContext.Contacts.ToListAsync();
+            return await _dbContext.Contacts.AsNoTracking().ToListAsync();
         }
 
-        public async Task DeleteAsync(string Id)
+        public async Task DeleteAsync(string id)
         {
-            // ToDo - Integrate with EF Core
-            //var itemToRemove = ContactsList.SingleOrDefault(r => r.MobilePhone == Id);
-            //if (itemToRemove != null)
-                //ContactsList.Remove(itemToRemove);
-            var itemToRemove = _dbContext.Contacts.SingleOrDefault(c => c.MobilePhone.Equals(Id));
-            if (itemToRemove != null)
+            var contact = _dbContext.Contacts.SingleOrDefault(c => c.MobilePhone.Equals(id));
+            if (contact != null)
             {
-                _dbContext.Contacts.Remove(itemToRemove);
+                _dbContext.Contacts.Remove(contact);
                 await _dbContext.SaveChangesAsync();
             }
         }
 
-        public async Task UpdateAsync(Contact item)
+        public async Task UpdateAsync(Contact contact)
         {
-            var itemToUpdate = _dbContext.Contacts.SingleOrDefault(c => c.MobilePhone.Equals(item.MobilePhone));
-            //await TryUpdate
-            if (itemToUpdate != null)
-            {
-                itemToUpdate.FirstName = item.FirstName;
-                itemToUpdate.LastName = item.LastName;
-                itemToUpdate.Company = item.Company;
-                itemToUpdate.JobTitle = item.JobTitle;
-                itemToUpdate.Email = item.Email;
-                itemToUpdate.MobilePhone = item.MobilePhone;
-                itemToUpdate.DateOfBirth = item.DateOfBirth;
-                _context.Entry(itemToUpdate).State = EntityState.Modified;
-                _context.Contacts.Update(itemToUpdate);
-                await _context.SaveChangesAsync();
-            }
+            _dbContext.Entry(contact).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
