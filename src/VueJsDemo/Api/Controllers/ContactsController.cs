7d09c49ad0e7eb61e62250a979bfbacb2e3cbf09
Adam Anderly <adam@anderly.com>
2017-01-17 14:39:26 -0600

    Made controller more DRY using new filters.

--- src/VueJsDemo/Api/Controllers/ContactsController.cs
+++ src/VueJsDemo/Api/Controllers/ContactsController.cs
@@ -1,124 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using VueJsDemo.Api.Infrastructure;
+using VueJsDemo.Api.Filters;
 using VueJsDemo.Api.Models;
 using VueJsDemo.Api.Repository;
-using WebApi2.Infrastructure;
 
 namespace VueJsDemo.Api.Controllers
 {
     [Authorize]
+    [ValidateModel]
     [Route("api/[controller]")]
     public class ContactsController : Controller
     {
-        public IContactsRepository ContactsRepository { get; set; }
+        private readonly IContactsRepository _contactsRepository;
 
         public ContactsController(IContactsRepository contactsRepository)
         {
-            ContactsRepository = contactsRepository;
+            _contactsRepository = contactsRepository;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Contact>> GetAllAsync()
+        public async Task<IEnumerable<Contact>> Get()
         {
-            return await ContactsRepository.GetAllAsync();
+            return await _contactsRepository.ListAsync();
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> List()
-        //{
-        //    return Ok(Repository.GetAllAsync());
-        //}
-
         [HttpGet("{id}", Name = "GetContacts")]
-        public async Task<IActionResult> GetById(string id)
+        public async Task<IActionResult> Get(string id)
         {
-            var item = await ContactsRepository.FindAsync(id);
-            if (item == null)
-            {
-                return NotFound();
-            }
-            return new ObjectResult(item);
+            return Ok(await _contactsRepository.GetByIdAsync(id));
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Contact item)
+        public async Task<IActionResult> Post([FromBody] Contact contact)
         {
-            try
-            {
-                if (item == null || !ModelState.IsValid)
-                {
-                    return BadRequest(ErrorCode.FailedValidation.ToString());
-                }
-                await ContactsRepository.AddAsync(item);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new Error
-                {
-                    Code = ErrorCode.CouldNotCreateItem.ToString(),
-                    Message = ex.InnerException.Message
-                });
-            }
-            //return Ok(item);
-            return CreatedAtRoute("GetContacts", new { Controller = "Contacts", id = item.MobilePhone }, item);
+            await _contactsRepository.AddAsync(contact);
+            return CreatedAtRoute("GetContacts", new { Controller = "Contacts", id = contact.MobilePhone }, contact);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(string id, [FromBody] Contact item)
+        [ValidateContactExists]
+        public async Task<IActionResult> Put(string id, [FromBody] Contact contact)
         {
-            //if (item == null)
-            //{
-            //    return BadRequest();
-            //}
-            //var contactObj = await ContactsRepository.FindAsync(id);
-            //if (contactObj == null)
-            //{
-            //    return NotFound();
-            //}
-            //await ContactsRepository.UpdateAsync(item);
-            //return new NoContentResult();
-            try
-            {
-                if (item == null || !ModelState.IsValid)
-                {
-                    return BadRequest(ErrorCode.FailedValidation.ToString());
-                }
-                var existingItem = await ContactsRepository.FindAsync(id);
-                if (existingItem == null)
-                {
-                    return NotFound(ErrorCode.RecordNotFound.ToString());
-                }
-                await ContactsRepository.UpdateAsync(item);
-            }
-            catch (Exception)
-            {
-                return BadRequest(ErrorCode.CouldNotUpdateItem.ToString());
-            }
-            return NoContent();
+            await _contactsRepository.UpdateAsync(contact);
+            return Ok(contact);
         }
 
         [HttpDelete("{id}")]
+        [ValidateContactExists]
         public async Task<IActionResult> Delete(string id)
         {
-            try
-            {
-                var item = ContactsRepository.FindAsync(id);
-                if (item == null)
-                {
-                    return NotFound(ErrorCode.RecordNotFound.ToString());
-                }
-                await ContactsRepository.RemoveAsync(id);
-            }
-            catch (Exception)
-            {
-                return BadRequest(ErrorCode.CouldNotDeleteItem.ToString());
-            }
+            await _contactsRepository.DeleteAsync(id);
             return NoContent();
-
         }
     }
+
 }
